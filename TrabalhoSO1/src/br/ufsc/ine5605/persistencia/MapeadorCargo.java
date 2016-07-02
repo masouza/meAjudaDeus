c4665d279acaf288a3c47810f9d95cd07cf94ab7
masouza <masouza201997@gmail.com>
2016-07-01 21:41:51 -0300

    Add files via upload

--- TrabalhoSO1/src/br/ufsc/ine5605/persistencia/MapeadorCargo.java
+++ TrabalhoSO1/src/br/ufsc/ine5605/persistencia/MapeadorCargo.java
@@ -5,24 +5,90 @@
  */
 package br.ufsc.ine5605.persistencia;
 
+import br.ufsc.ine5605.Controladores.ControladorFuncionario;
 import br.ufsc.ine5605.codigos.Cargo;
+import br.ufsc.ine5605.codigos.Funcionario;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 /**
  *
- * @author Aluno2
+ * @author NEGO BAM
  */
 public class MapeadorCargo {
 
-    private HashMap<Integer, Cargo> cargos = new HashMap<>();
+    private ControladorFuncionario owner;
+    private HashMap<Integer, Cargo> cargos;
     private final String filename = "candidatos.dat";
 
-    public Cargo get(Integer idCargo) {
-        return cargos.get(idCargo);
+    public MapeadorCargo(ControladorFuncionario owner) {
+        this.owner = owner;
+        this.cargos = owner.getCargos();
     }
 
-    public void put(Cargo cargo) {
-        cargos.put(cargo.getCodigo(), cargo);
+    public void setCargo() {
+        this.cargos = owner.getCargos();
+    }
+    
+    public HashMap getCargos(){
+        return cargos;
     }
 
+    public void persist() {
+        setCargo();
+        try {
+            FileOutputStream fout = new FileOutputStream(filename);
+            
+            ObjectOutputStream oo = new ObjectOutputStream(fout);
+            oo.writeObject(cargos);
+            
+            oo.flush();
+            fout.flush();
+            
+            oo.close();
+            fout.close();
+            oo = null;
+            fout = null;
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Ops, ocorreu o erro: "+ ex);
+        } catch (IOException ex) {
+            System.out.println("Ops, ocorreu o erro" + ex);
+        }
+
+    }
+
+    public void load(){
+        
+        try{
+            FileInputStream fin = new FileInputStream(filename);
+            ObjectInputStream oi = new ObjectInputStream(fin);
+            
+            this.cargos = (HashMap<Integer, Cargo>) oi.readObject();
+            
+            oi.close();
+            fin.close();
+            oi  = null;
+            fin = null;
+            
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("Ops, ocorreu o erro: "+ex);
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("Ops, ocorreu o erro: "+ex);
+        }
+        catch(IOException ex){
+            System.out.println("Ops, ocorreu o erro: "+ex);
+        }
+        owner.setCargos(cargos);
+    }
+    
+    
 }
--- TrabalhoSO1/src/br/ufsc/ine5605/persistencia/MapeadorFuncionario.java
+++ TrabalhoSO1/src/br/ufsc/ine5605/persistencia/MapeadorFuncionario.java
@@ -5,24 +5,89 @@
  */
 package br.ufsc.ine5605.persistencia;
 
+import br.ufsc.ine5605.Controladores.ControladorFuncionario;
+import br.ufsc.ine5605.codigos.Cargo;
 import br.ufsc.ine5605.codigos.Funcionario;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 /**
  *
- * @author Aluno2
+ * @author NEGO BAM
  */
 public class MapeadorFuncionario {
 
-    private HashMap<Integer, Funcionario> funcionarios = new HashMap<>();
+    
+    private ControladorFuncionario owner;
+    private HashMap<Integer, Funcionario> funcionarios;
     private final String filename = "candidatos.dat";
 
-    public Funcionario get(Integer idFuncionario) {
-        return funcionarios.get(idFuncionario);
+    public MapeadorFuncionario(ControladorFuncionario owner) {
+        this.owner = owner;
+        this.funcionarios = owner.getFuncionarios();
+    }
+    
+    public void setFuncionarios(){
+        this.funcionarios = owner.getFuncionarios();
+    }
+    public HashMap getFuncionarios(){
+        return funcionarios;
+    }
+    
+    public void persist() {
+         setFuncionarios();
+        try {
+            FileOutputStream fout = new FileOutputStream(filename);
+            
+            ObjectOutputStream oo = new ObjectOutputStream(fout);
+            oo.writeObject(funcionarios);
+            
+            oo.flush();
+            fout.flush();
+            
+            oo.close();
+            fout.close();
+            oo = null;
+            fout = null;
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Ops, ocorreu o erro: "+ ex);
+        } catch (IOException ex) {
+            System.out.println("Ops, ocorreu o erro" + ex);
+        }
+
     }
 
-    public void put(Funcionario funcionario) {
-        funcionarios.put(funcionario.getMatricula(), funcionario);
+    public void load(){
+        
+        try{
+            FileInputStream fin = new FileInputStream(filename);
+            ObjectInputStream oi = new ObjectInputStream(fin);
+            
+            this.funcionarios = (HashMap<Integer, Funcionario>) oi.readObject();
+            
+            oi.close();
+            fin.close();
+            oi  = null;
+            fin = null;
+            
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("Ops, ocorreu o erro: "+ex);
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("Ops, ocorreu o erro: "+ex);
+        }
+        catch(IOException ex){
+            System.out.println("Ops, ocorreu o erro: "+ex);
+        }
+        owner.setFuncionarios(funcionarios);
     }
+
 
 }
