3137b0bdf66eac9c4c9fd5debe5bdc7fe18e88d3
masouza <masouza201997@gmail.com>
2016-07-01 21:42:56 -0300

    Add files via upload

--- TrabalhoSO1/src/br/ufsc/ine5605/Controladores/ControladorFuncionario.java
+++ TrabalhoSO1/src/br/ufsc/ine5605/Controladores/ControladorFuncionario.java
@@ -8,6 +8,8 @@ package br.ufsc.ine5605.Controladores;
 import br.ufsc.ine5605.codigos.Cargo;
 import br.ufsc.ine5605.codigos.Funcionario;
 import br.ufsc.ine5605.codigos.TipoCargo;
+import br.ufsc.ine5605.persistencia.MapeadorCargo;
+import br.ufsc.ine5605.persistencia.MapeadorFuncionario;
 import br.ufsc.ine5605.telas.TelaAddCargo;
 import br.ufsc.ine5605.telas.TelaAddFuncionario;
 import br.ufsc.ine5605.telas.TelaMostraCargos;
@@ -17,7 +19,7 @@ import java.util.HashMap;
 
 /**
  *
- * @author renan
+ * @author NEGO BAM
  */
 public class ControladorFuncionario {
 
@@ -28,6 +30,8 @@ public class ControladorFuncionario {
     private TelaMostraFuncionarios telaMostraFuncionarios;
     private TelaMostraCargos telaMostraCargos;
     private ControladorGeral owner;
+    private MapeadorCargo mapCargo;
+    private MapeadorFuncionario mapFunc;
 
     public ControladorFuncionario(ControladorGeral owner) {
         this.telaAddFuncionario = new TelaAddFuncionario(this);
@@ -35,6 +39,19 @@ public class ControladorFuncionario {
         this.telaMostraFuncionarios=new TelaMostraFuncionarios(this);
         this.telaMostraCargos=new TelaMostraCargos(this);
         this.owner = owner;
+        this.mapCargo = new MapeadorCargo(this);
+        this.mapFunc = new MapeadorFuncionario(this);
+        mapCargo.load();
+        mapFunc.load();
+        
+        if(mapCargo.getCargos() != null){
+            this.cargos = mapCargo.getCargos();
+        }
+        if(mapFunc.getFuncionarios() != null){
+            this.funcionarios = mapFunc.getFuncionarios();
+        }
+        
+       
     }
 
     public HashMap<Integer, Funcionario> getFuncionarios() {
@@ -43,6 +60,7 @@ public class ControladorFuncionario {
 
     public void setFuncionarios(HashMap<Integer, Funcionario> funcionarios) {
         this.funcionarios = funcionarios;
+        mapFunc.persist();
     }
 
     public HashMap<Integer, Cargo> getCargos() {
@@ -51,6 +69,7 @@ public class ControladorFuncionario {
 
     public void setCargos(HashMap<Integer, Cargo> cargos) {
         this.cargos = cargos;
+        mapCargo.persist();
     }
 
     public TelaAddFuncionario getTelaAddFuncionario() {
@@ -59,6 +78,7 @@ public class ControladorFuncionario {
 
     public void setTelaAddFuncionario(TelaAddFuncionario telaFuncionario) {
         this.telaAddFuncionario = telaFuncionario;
+        
     }
 
     public ControladorGeral getOwner() {
@@ -84,15 +104,16 @@ public class ControladorFuncionario {
     
     
     
-    public void addFuncionario(Integer matricula, String nome, long telefone, float salario, Date nascimento,Cargo cargo) {
+    public void addFuncionario(Integer matricula, String nome, long telefone, float salario, Date nascimento,TipoCargo cargo) {
         Funcionario funcionario = new Funcionario();
         funcionario.setMatricula(matricula);
         funcionario.setNome(nome);
         funcionario.setTelefone(telefone);
         funcionario.setSalario(salario);
         funcionario.setNascimento(nascimento);
-        funcionario.setCargo(cargo);
+        //funcionario.setCargo(cargo);
         funcionarios.put(matricula, funcionario);
+        mapFunc.persist();
     }
 
     public void addCargo(Integer codigo, String nome, TipoCargo tipoCargo){
@@ -101,6 +122,7 @@ public class ControladorFuncionario {
         cargo.setNome(nome);
         cargo.setTipoCargo(tipoCargo);
         cargos.put(codigo, cargo);
+        mapCargo.persist();
         
     }
 
